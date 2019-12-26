48ab73a045bc30604ea43f2a74ffa766e9c9e28d
leijmdas@163.com <leijmdas@163.com>
2019-12-26 18:12:29 +0800

    add

--- config/index.js
+++ config/index.js
@@ -29,17 +29,17 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                //target: 'http://120.78.136.63:10080',
-                target: 'http://localhost:10080',
-                 //target: 'http://localhost:8085',
+                target: 'http://120.78.136.63:10080',
+                //target: 'http://localhost:10080',
+                //target: 'http://localhost:8085',
                 changeOrigin: true, // 如果接口跨域，需要进行这个参数配置,
                 pathRewrite: {
                     '^/api': '/'   // 重写接口
                 }
             },
             '/dongxw': {
-                //target: 'http://120.78.136.63:10081/dongxw', // 接口的域名
-                target: 'http://localhost:10081/dongxw', // 接口的域名
+                target: 'http://120.78.136.63:10081/dongxw', // 接口的域名
+                //target: 'http://localhost:10081/dongxw', // 接口的域名
                 secure: false,  // 如果是https接口，需要配置这个参数
                 changeOrigin: true, // 如果接口跨域，需要进行这个参数配置,
                 pathRewrite: {
